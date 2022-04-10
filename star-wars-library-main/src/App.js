fa8f333f3c91e1b36df47510a16b4fde3fa179ee
Alif Murti <alifmurti86@gmail.com>
2022-04-10 12:07:32 +0700

    done

---
+++ star-wars-library-main/src/App.js
@@ -0,0 +1,23 @@
+// menggunakan State karena ini mini app, tidak perlu Context
+import React from "react";
+import './main.css'
+import Header from "./Components/Header/Header"
+import {BrowserRouter, Route, Routes} from 'react-router-dom'
+import Home from "./Components/Pages/Home";
+import Movie from "./Components/Pages/Movie";
+
+const App = () => {
+  return (
+    <div className="app_container">
+      <BrowserRouter>
+        <Header />
+        <Routes>
+          <Route path="/" element={<Home />}/>
+          <Route path="/movie/:id" element={<Movie />}/>
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
+}
+
+export default App;
